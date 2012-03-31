7fa94bbf50a3eb5321ca67bb367510fff2553efd
Lars Grammel <lars.grammel@gmail.com>
2012-03-31 16:25:12 -0700

    experimental work on stream chart

--- d3/chart/stream/stream_chart.js
+++ d3/chart/stream/stream_chart.js
@@ -4,6 +4,8 @@ d3.chart.stream = function() {
     chart.width = d3c_property(960);
     chart.height = d3c_property(500);
 
+    chart.key = d3c_property(undefined);
+
     var stack =  d3.layout.stack().offset("wiggle");
     
     var _attr = [];
@@ -20,10 +22,13 @@ d3.chart.stream = function() {
     
     function chart(selection) {
         selection.each(function(data) {
+            var chartElement = this;
+
             // get values from properties
             var margin = chart.margin();
             var height = chart.height();
             var width = chart.width();
+            var key = chart.key();
 
             // Select the svg element, if it exists.
             var svg = d3.select(this).selectAll("svg").data([data]);
@@ -57,21 +62,85 @@ d3.chart.stream = function() {
                 .y0(function(d) { return availableHeight - d.y0 * availableHeight / my; })
                 .y1(function(d) { return availableHeight - (d.y + d.y0) * availableHeight / my; });
 
-            var layer = g.selectAll("path").data(stackedData);
-            
+            // old data
+            var oldChartInstance = chartElement.__CHART__;
+            var oldData = d3.range(stackedData.length);
+            if (oldChartInstance === undefined) {
+                // begin in the middle and expand
+                var n = stackedData[0].length
+                var defaultEntries = d3.range(n).map(function(i) {
+                    return {
+                        'x': i,
+                        'y': 0,
+                        'y0': my / 2
+                    }
+                });
+                oldData = oldData.map(function() {
+                    return defaultEntries;
+                });
+            } else {
+                // top line of current chart, 0 height
+                var defaultEntries = oldChartInstance.data[oldChartInstance.data.length - 1]
+                    .map(function(d) {
+                    return {
+                        'x': d.x,
+                        'y': 0,
+                        'y0': d.y0
+                    }
+                });
+
+                oldData = oldData.map(function(i) {
+                    return (i < oldChartInstance.data.length) ?
+                            oldChartInstance.data[i] :
+                            defaultEntries;
+                });
+            }
+
+            var layer = g.selectAll("path").data(stackedData, key);
+
             // enter
             var layerEnter = layer.enter().append("path").classed("layer", true);
+            // register event handlers
             layerEnter.call(function(d) {
                 var _this = this;
                 _on.forEach(function(args) {
                     layerEnter.on.apply(_this, args);
                 });
             });
+            // start with the final values for color etc.
+            layerEnter.call(function(d) {
+                var _this = this;
+                _attr.forEach(function(args) {
+                    d.attr.apply(_this, args);
+                });
+            }, layer);
+            // if there is a current chartInstance, add on top, otherwise default path
+            layerEnter.call(function(e) {
+                e.attr("d", function(d, i) {
+                    return area(oldData[i]);
+                });
+            });
 
-            // update
+            // update - tween d on y, y0 only
             var layerUpdate = d3.transition(layer);
-            layerUpdate.attr("d", area);
-            layerUpdate.call(function(d) { 
+            layerUpdate.attrTween("d", function(d, i, a) {
+                // assert: originalData.length == targetData.length
+                var originalData = oldData[i]; // TODO rescale ( * (oldMy / currentMy)
+                var targetData = d;
+                var n = targetData.length;
+                return function(t) {
+                    var currentData = [];
+                    for (var i=0; i < n; i++) {
+                        currentData[i] = {
+                            'x': targetData[i].x,
+                            'y': originalData[i].y + t * (targetData[i].y - originalData[i].y),
+                            'y0': originalData[i].y0 + t * (targetData[i].y0 - originalData[i].y0)
+                        };
+                    }
+                    return area(currentData);
+                }
+            });
+            layerUpdate.call(function(d) {
                 var _this = this;
                 _attr.forEach(function(args) {
                     d.attr.apply(_this, args);
@@ -79,18 +148,21 @@ d3.chart.stream = function() {
             }, layer);
             
             // exit
-            var layerExit = d3.transition(layer.exit()).remove();
+            layer.exit().remove();
+            // var layerExit = d3.transition(layer.exit()).remove();
             
             // store the chart in the layer and root node DOM elements
             var chartInstance = {
-                'root': this
+                'root': this,
+                'data': stackedData,
+                'area': area
             };
 
             layerEnter.each(function() {
                 this.__CHART__ = chartInstance;
             });
 
-            this.__CHART__ = chartInstance;
+            chartElement.__CHART__ = chartInstance;
         });
     }
 
